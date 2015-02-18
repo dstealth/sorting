6880212bb6f555b18043aaa0605ba61cc221efd8
Dima_Stelmah <dmytriistelmakh@gmail.com>
2015-02-18 22:02:42 +0200

    Selection sorting final

--- SelectionSort.php
+++ SelectionSort.php
@@ -1,27 +1,33 @@
 <?php
 define("SIZE", 10);
 
+//$a = array(32, 47, 33, 48, 76, 75, 81, 70, 93, 96);
+//$a = array(32, 47, 33, 48, 76, 75, 81, 70, 93, 96);
 $a=array();
-for ($i=0;$i<SIZE;$i++){
-    $a[$i]=rand(1, 99);
+for ($i = 0; $i < SIZE; $i++) {
+    $a[$i] = rand(1, 99);
     echo $a[$i] . " ";
 }
+//print_r($a);
 ?>
-<br/>
-<br/>
-<br/>
-
+    <br/>
+    <br/>
+    <br/>
 <?php
-$max=0;
-for ($i=0; $i<SIZE; $i++){
-    for ($j=1; $j<SIZE; $j++){
-       if ($a[$j]<$a[$max]){
-           $k=$j;
-           $j=$max;
-           $max=$k;
-       }
+for ($i = 0, $min = 0; $i < SIZE; $i++, $min=$i) {
+    for ($j = $i+1; $j < SIZE; $j++) {
+        if ($a[$j] < $a[$min]) {
+            $min = $j;
+//            echo $min . " <br/>";
+        }
     }
-//    $max=-1;
+//    echo "a[i]=" . $a[$i] . " -- a[min]=" . $a[$min] . " <br/>";
+    $k = $a[$i];
+    $a[$i] = $a[$min];
+    $a[$min] = $k;
+//    echo "a[i]= $a[$i] -- a[min]=$a[$min] <br/>";
+//    echo "========================<br/>";
 }
-var_dump($a);
+//var_dump($a);
+print_r($a);
 ?>
