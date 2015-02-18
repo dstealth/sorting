70bd9782ab3c43931ffd5df9e00db64dd02379e9
Dima_Stelmah <dmytriistelmakh@gmail.com>
2015-02-18 16:12:35 +0200

    Insertion sorting create

--- InsertionSort.php
+++ InsertionSort.php
@@ -1,3 +1,25 @@
 <?php
+define("SIZE", 10);
+
+$a = array();
+for ($i = 0; $i < SIZE; $i++) {
+    $a[$i] = rand(1, 99);
+    echo $a[$i] . " ";
+}?>
+<br/>
+<br/>
+<br/>
+
+<?php
+for ($i = 1; $i < SIZE; $i++) {
+    for ($j = $i; $j != 0; $j--) {
+        if ($a[$j] < $a[$j - 1]) {
+            $k = $a[$j];
+            $a[$j] = $a[$j - 1];
+            $a[$j - 1] = $k;
+        }
+    }
+}
+var_dump($a);
 
 ?>
