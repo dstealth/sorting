c0046d323048680a568b8f51013bebf0a4e18710
Dima_Stelmah <dmytriistelmakh@gmail.com>
2015-02-18 14:39:02 +0200

    Final version

--- HairbrushSort.php
+++ HairbrushSort.php
@@ -10,17 +10,6 @@ function Q($j)
     return $var2;
 }
 
-echo Q(0) . "<br/>";
-echo Q(1) . "<br/>";
-echo Q(2) . "<br/>";
-echo Q(3) . "<br/>";
-echo Q(4) . "<br/>";
-echo Q(5) . "<br/>";
-echo Q(6) . "<br/>";
-echo Q(7) . "<br/>";
-echo Q(8) . "<br/>";
-echo Q(9) . "<br/>";
-
 $a = array();
 for ($i = 0; $i < SIZE; $i++) {
     $a[$i] = rand(1, 99);
@@ -33,18 +22,19 @@ for ($i = 0; $i < SIZE; $i++) {
 <?php
 for ($i = 0; $i < SIZE; $i++) {
     for ($j = 0; $j < (SIZE - 1); $j++) {
-        if ($j > Q($j)) {
-            if ($a[$j] > ($a[Q($i)+$j])) {
-                $k = $a[$j];
-                $a[$j] = $a[$j+Q($i)];
-                $a[$j+Q($i)] = $k;
-            }
-        }
-        else {
-            if ($a[$j] > $a[$j + 1]) {
-                $k = $a[$j];
-                $a[$j] = $a[$j + 1];
-                $a[$j + 1] = $k;
+        if (Q($i) + $j < SIZE) {
+            if ($j > Q($j)) {
+                if ($a[$j] > ($a[Q($i) + $j])) {
+                    $k = $a[$j];
+                    $a[$j] = $a[$j + Q($i)];
+                    $a[$j + Q($i)] = $k;
+                }
+            } else {
+                if ($a[$j] > $a[$j + 1]) {
+                    $k = $a[$j];
+                    $a[$j] = $a[$j + 1];
+                    $a[$j + 1] = $k;
+                }
             }
         }
     }
