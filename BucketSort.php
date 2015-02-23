b4fd36a42e73a4e200ca179de6045ab9dd54aeb4
Dima_Stelmah <dmytriistelmakh@gmail.com>
2015-02-23 21:52:30 +0200

    V 0.0.1 or first try

--- BucketSort.php
+++ BucketSort.php
@@ -1,3 +1,27 @@
 <?php
+define("SIZE", 10);
 
+$a = array();
+for ($i = 0; $i < SIZE; $i++) {
+    $a[$i] = rand(1, 99);
+    echo $a[$i] . " ";
+}
+?>
+    <br/>
+    <br/>
+    <br/>
+<?php
+for ($i = 0; $i < SIZE; $i++) {
+    for ($j = 0; $j < SIZE; $j++) {
+        if ($a[$i] > 0 && $a[$i] <= 25) {
+            $a[$i];
+        } elseif ($a[$i] > 25 && $a[$i] <= 50) {
+            $a[$i];
+        } elseif ($a[$i] > 50 && $a[$i] <= 75) {
+            $a[$i];
+        } else {
+            $a[$i];
+        }
+    }
+}
 ?>
