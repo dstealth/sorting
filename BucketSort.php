6e939035323800bb955a17664621d9f584741a92
Dima_Stelmah <dmytriistelmakh@gmail.com>
2015-02-24 21:06:03 +0200

    V.0.0.2

--- BucketSort.php
+++ BucketSort.php
@@ -1,27 +1,29 @@
 <?php
-define("SIZE", 10);
+define("SIZE", 50);
 
 $a = array();
 for ($i = 0; $i < SIZE; $i++) {
-    $a[$i] = rand(1, 99);
+    $a[$i] = rand(0, 99);
     echo $a[$i] . " ";
 }
 ?>
     <br/>
     <br/>
     <br/>
+<h3>1 - 49:</h3>
 <?php
 for ($i = 0; $i < SIZE; $i++) {
-    for ($j = 0; $j < SIZE; $j++) {
-        if ($a[$i] > 0 && $a[$i] <= 25) {
-            $a[$i];
-        } elseif ($a[$i] > 25 && $a[$i] <= 50) {
-            $a[$i];
-        } elseif ($a[$i] > 50 && $a[$i] <= 75) {
-            $a[$i];
-        } else {
-            $a[$i];
-        }
+    if ($a[$i] > 0 && $a[$i] < 50) {
+        echo $a[$i] . " ";
+    }
+} ?>
+    <br/>
+    <br/>
+    <h3>50 - 99:</h3>
+<?php
+for ($i = 0; $i < SIZE; $i++) {
+    if ($a[$i] >= 50 && $a[$i] < 100) {
+        echo $a[$i] . " ";
     }
 }
 ?>
